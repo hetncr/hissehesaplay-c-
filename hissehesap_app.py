323516894bd3cf25d8d6011999a5633f1fc53b2d
hetncr <97675352+hetncr@users.noreply.github.com>
2024-01-20 15:15:22 +0300

    Update hissehesap_app.py

--- hissehesap_app.py
+++ hissehesap_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 #streamlit.config.theme.base = "dark"
 st.title("Hocalar Hisse Hesaplıyor")
-st.subheader("Road to Kıbrıs")
+st.subheader("Road to Kıbrıs :sunglasses:")
 #st.set_page_config(
 # page_title="Hisse Hedef Fiyat Hesaplayıcı",
 #  page_icon="https://example.com/icon.png",
